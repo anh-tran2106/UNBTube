b6e9d4ed6306d9b7fa48ed390286127ece82a963
wthomasUNB <155482445+wthomasUNB@users.noreply.github.com>
2025-03-25 19:19:01 -0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,13 @@
+#/usr/bin/env python
+APP_HOST = 'cs3103.cs.unb.ca'
+APP_PORT = 
+APP_DEBUG = True
+
+DB_HOST = 'localhost'
+DB_USER = ''
+DB_PASSWD = ''
+DB_DATABASE = ''
+
+SECRET_KEY = 'd41d8cd98f00b204e9800998ecf8427e'
+
+LDAP_HOST =  'ldap-student.cs.unb.ca'
