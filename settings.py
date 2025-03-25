a86e3513354c282ccce68fef1426da29b3eb0c22
wthomasUNB <155482445+wthomasUNB@users.noreply.github.com>
2025-03-25 19:24:02 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 #/usr/bin/env python
 APP_HOST = 'cs3103.cs.unb.ca'
-APP_PORT = 8000
+APP_PORT = 8026
 APP_DEBUG = True
 
 DB_HOST = 'localhost'
-DB_USER = ''
-DB_PASSWD = ''
+DB_USER = 'r5r4r'
+DB_PASSWD = 'NFbLza10'
 DB_DATABASE = ''
 
 SECRET_KEY = 'd41d8cd98f00b204e9800998ecf8427e'
