a971d1862419f21f5c798cf24282c0f3e43c60eb
wthomasUNB <155482445+wthomasUNB@users.noreply.github.com>
2025-03-29 00:28:07 -0300

    Current state of python script to create users. I'm looking into using a python telnet library to send the verification email

--- cgi-bin/createUser.py
+++ cgi-bin/createUser.py
@@ -2,6 +2,8 @@ import pymysql
 import settings
 import os
 import json
+import random
+from hashlib import sha256
 
 dbConnection = pymysql.connect(host=settings.DB_HOST,
                                 user=settings.DB_USER,
@@ -11,5 +13,20 @@ dbConnection = pymysql.connect(host=settings.DB_HOST,
                                 cursorclass= pymysql.cursors.DictCursor)
 
 class CreateUser():
-    def CreateUser(self):
+    def CreateUser(self, username, email, password):
+        salt = ""
+        chars=[]
+        alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        for i in range(16):
+            chars.append(random.choice(alphabet))
+            salt = "".join(chars)
         
+        hashPass = sha256((salt.encode('utf-8') + password.encode('utf-8'))).hexdigest()
+        sqlProc = 'createUser'
+        sqlArgs = [username, email, hashPass, salt,]
+        try:
+            cursor = dbConnection.cursor()
+            cursor.callproc(sqlProc, sqlArgs)
+
+
+
