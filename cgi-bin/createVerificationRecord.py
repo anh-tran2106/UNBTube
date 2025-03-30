b71d45144c98ebf96de957ee0ffacf915da7b7ec
wthomasUNB <155482445+wthomasUNB@users.noreply.github.com>
2025-03-29 21:44:48 -0300

    Working python scripts which can create User and userVerification records

--- cgi-bin/createVerificationRecord.py
+++ cgi-bin/createVerificationRecord.py
@@ -4,11 +4,10 @@ import os
 import json
 import random
 from hashlib import sha256
-from createVerificationRecord import createVRec
 
 
 
-def CreateUser(username, email, password):
+def createVRec(email):
     dbConnection = pymysql.connect(host=settings.DB_HOST,
                             user=settings.DB_USER,
                             password=settings.DB_PASSWD,
@@ -21,23 +20,24 @@ def CreateUser(username, email, password):
     for i in range(16):
         chars.append(random.choice(alphabet))
         salt = "".join(chars)
-    print("Got here")
-    hashPass = sha256((salt.encode('utf-8') + password.encode('utf-8'))).hexdigest()
-    sqlProc = 'createUser'
-    sqlArgs = [username, email, hashPass, salt,]
+    
+    hashEmail = sha256((salt.encode('utf-8') + email.encode('utf-8'))).hexdigest()
+    sqlProc = 'getUID'
+    sqlArgs = [email,]
     try:
-        #cursor = dbConnection.cursor()
-        #cursor.callproc(sqlProc, sqlArgs)
-        #dbConnection.commit()
-        createVRec("New12@email7.com")
+        cursor = dbConnection.cursor()
+        cursor.callproc(sqlProc, sqlArgs)
+        dbConnection.commit()
+        data = cursor.fetchone()
+        uid = data["userID"]
+        sqlProc = 'createemail'
+        sqlArgs = [uid,hashEmail,]
+        cursor.callproc(sqlProc,sqlArgs,)
+        dbConnection.commit()
     except pymysql.MySQLError as e:
         return {"Status": 400, "Message": "A MySQL Error has occured\nSQL Error Message: {e}"}
     except Exception as e:
         return {"Status": 500, "Message": "A general error has occured.\nError message: {e}"}
     finally:
-        #cursor.close()
+        cursor.close()
         dbConnection.close()
-
-
-
-
