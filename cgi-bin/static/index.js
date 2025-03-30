70902efbb0c616bbb6d5f2e31691e711384a8f04
anh-tran2106 <tranlamtuananh2003@gmail.com>
2025-03-30 20:18:27 -0300

    add getVideos method to homepage

--- cgi-bin/static/index.js
+++ cgi-bin/static/index.js
@@ -1,29 +1,45 @@
 var app = new Vue({
-    el: "#app",
-  
-    //------- data --------
-    data: {
-      serviceURL: "https://cs3103.cs.unb.ca:8026",
-      authenticated: false,
-      loggedIn: null,
-      input: {
-        username: "",
-        password: ""
-      }
-    },
-    methods: {
-      logout() {
-        axios
-        .delete(this.serviceURL+"/logout")
-        .then(response => {
-            location.reload();
-        })
-        .catch(e => {
-          console.log(e);
-        });
-      },
+  el: "#app",
+
+  //------- data --------
+  data: {
+    serviceURL: "https://cs3103.cs.unb.ca:8027",
+    authenticated: false,
+    loggedIn: null,
+    videos: [], // Store fetched videos
+    input: {
+      username: "",
+      password: ""
     }
-    //------- END methods --------
-  
-  });
-  
+  },
+
+  methods: {
+    logout() {
+      axios
+      .delete(this.serviceURL + "/logout")
+      .then(response => {
+          location.reload();
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    },
+
+    // Fetch all videos from the /video endpoint
+    getVideos() {
+      axios
+      .get(this.serviceURL + "/videos")
+      .then(response => {
+        this.videos = response.data.videos; // Assuming API returns { videos: [...] }
+      })
+      .catch(e => {
+        console.log("Error fetching videos:", e);
+      });
+    }
+  },
+
+  // Fetch videos when the component is created
+  created() {
+    this.getVideos();
+  }
+});
