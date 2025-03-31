7abf3058f3801258d1bfaf1e491d22f798135746
wthomasUNB <155482445+wthomasUNB@users.noreply.github.com>
2025-03-30 21:48:49 -0300

    Created getAllComments.py file

---
+++ cgi-bin/getAllComments.py
@@ -0,0 +1,26 @@
+import pymysql
+import settings
+
+def getAllComments(vidID):
+    dbConnection = pymysql.connect(host=settings.DB_HOST,
+                        user=settings.DB_USER,
+                        password=settings.DB_PASSWD,
+                        database=settings.DB_DATABASE,
+                        charset='utf8mb4',
+                        cursorclass= pymysql.cursors.DictCursor)
+    sqlProc = "getVideoComments"
+    sqlArgs = [vidID,]
+    data = ""
+    try:
+        cursor = dbConnection.cursor()
+        cursor.callproc(sqlProc, sqlArgs)
+        dbConnection.commit()
+        data = cursor.fetchall()
+    except pymysql.MySQLError as e:
+        return {"Status": 400, "Message": "A MySQL Error has occured", "Error Message": e}
+    except Exception as e:
+        return {"Status": 500, "Message": "A general error has occured.\nError message: {e}"}
+    finally:
+        cursor.close()
+        dbConnection.close()
+    return data
