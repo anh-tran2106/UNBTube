837dab4f0a757200317250af04ccb304cb89541f
wthomasUNB <155482445+wthomasUNB@users.noreply.github.com>
2025-04-01 19:03:48 -0300

    Added findVideos.py

---
+++ cgi-bin/findVideos.py
@@ -0,0 +1,27 @@
+import pymysql
+import settings
+
+def findVideos(search_term):
+    dbConnection = pymysql.connect(host=settings.DB_HOST,
+                        user=settings.DB_USER,
+                        password=settings.DB_PASSWD,
+                        database=settings.DB_DATABASE,
+                        charset='utf8mb4',
+                        cursorclass= pymysql.cursors.DictCursor)
+    sqlProc = "getVideosByTitle"
+    sqlProc = search_term
+    data = ""
+    try:
+        cursor = dbConnection.cursor()
+        cursor.callproc(sqlProc)
+        dbConnection.commit()
+        data = cursor.fetchall()
+    except pymysql.MySQLError as e:
+        return {"Status": 400, "Message": "A MySQL Error has occured", "Error Message": e}
+    except Exception as e:
+        return {"Status": 500, "Message": "A general error has occured.\nError message: {e}"}
+    finally:
+        cursor.close()
+        dbConnection.close()
+    return data
+
