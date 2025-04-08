e64a5e5138f2f067bba233875cff7f1274dbfdf8
wthomasUNB <155482445+wthomasUNB@users.noreply.github.com>
2025-04-08 18:13:45 -0300

    Fixed search bug

--- cgi-bin/static/index.js
+++ cgi-bin/static/index.js
@@ -43,7 +43,20 @@ var app = new Vue({
       .get(this.serviceURL + "/search?s=" + this.input.searchTerm)
       .then(response => {
         console.log("Fetched videos:", response.data);
-        this.videos = response.data
+        this.videos = null;
+        this.videos = response.data;
+        this.$nextTick(() => {
+          const videos = this.$refs.videoPlayers;
+          if (Array.isArray(videos)) {
+            videos.forEach(video => {
+              if (video && typeof video.load === 'function') {
+                video.load();
+              }
+            });
+          } else if (videoElements && typeof videoElements.load === 'function') {
+            videos.load();
+          }
+        });
       })
       .catch(e =>{
         console.log("Error fetching videos:", e);
